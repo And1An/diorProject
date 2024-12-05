6d49ec5cc399d5d06971a27b72cc56af48d11cdc
And1An <138055148+And1An@users.noreply.github.com>
2024-12-05 21:42:05 +0200

    upd

--- js/main.js
+++ js/main.js
@@ -8,8 +8,9 @@ const swiper = new Swiper('.top__swiper', {
     },
 
 
-
-
+    pagination: {
+        el: ".swiper-pagination",
+    },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
